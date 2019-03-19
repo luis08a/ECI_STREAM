451ad7b2619b573e251b26b3e0b9e52ff52befcd
luis <luis.moreno-a@mail.escuelaing.edu.co>
2019-03-18 23:09:55 -0500

    ~

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,5 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var loginModule = (function () {
+    var logIn = function () {
+        let email = $("#inputEmail")
 
+        console.log("This is the login information",email)
+        alert(email)
+    }
+    return{
+        log: logIn
+    }    
+})();
 
--- src/main/resources/static/js/query.js
+++ src/main/resources/static/js/query.js
@@ -8,7 +8,7 @@ var queryModule = {
             $("#table tbdoy").append(markup)
         }
         return {
-            queryUsers: getUsers
+            queryUsers:getUsers
         }
     }
 }
