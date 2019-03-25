a19ce8b43cca5dc7cbdd9a44d47ce9e79ffb1df8
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-03-25 00:58:06 -0500

    room form data take

--- src/main/resources/static/js/room.js
+++ src/main/resources/static/js/room.js
@@ -1,31 +1,33 @@
 var roomModule = (function () {
     
     var createRoom =function () {
-        /*let roomInfo = {category:$("#category").val(),
+        let roomInfo = {
+            category:$("#category").val(),
             title:$("#title").val(),
             keyWords:[],
             description:$("#description").val()
-        }*/
-        let roomInfo={ category:"maths",title:"room2",keyWords:[],description:"d"}
+        }
+        //console.log("data: "+roomInfo.title+" "+ roomInfo.category +" "+ roomInfo.description)
+        /*let roomInfo={ category:"maths",title:"room2",keyWords:[],description:"d"}*/
         event.preventDefault();
-        console.log("Hola")
-            APIModule.getUserById("user1",function (data) {
-                console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLll")
-                console.log(data)
-                $.ajax({
-                    url: '/api/rooms',
-                    type: 'POST',
-                    dataType: 'json',
-                    contentType: 'application/json',
-                    success: function (succes) {
-                        console.log('room created');
-                    },
-                    data: JSON.stringify({data,roomInfo})
-                })
-                
-            })
+        //console.log("Hola")
+        APIModule.getUserById("user1",function (data) {
+            //console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLll")
+            //console.log(data)
+            $.ajax({
+                url: '/api/rooms',
+                type: 'POST',
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (succes) {
+                    console.log('room created');
+                },
+                data: JSON.stringify({data,roomInfo})
+            });
+            console.log(JSON.stringify({data,roomInfo}));
+        });
         
-    }
+    };
     var getRooms = function () {
         APIModule.getRooms( function(data) {
             data.forEach( value => {
