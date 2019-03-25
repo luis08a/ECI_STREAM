308193fffda008be0339a472c669e405af583940
luis <luis.moreno-a@mail.escuelaing.edu.co>
2019-03-25 17:27:24 -0500

    despliegue heroku

--- src/main/java/edu/eci/arsw/eci_stream/security/AppSecurityConfig.java
+++ src/main/java/edu/eci/arsw/eci_stream/security/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,11 +21,18 @@ public class AppSecurityConfig	 extends WebSecurityConfigurerAdapter {
 		auth.authenticationProvider(authProvider);
 	}
     
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/resources/**")
+            .antMatchers("/**.css");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/register","/","/api/users/**","api/rooms/**","register.html").permitAll()
+                .antMatchers("/","/register").permitAll()
                 //.anyRequest().authenticated()
                 .and()
             .formLogin()
