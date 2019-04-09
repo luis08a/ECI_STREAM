dcfe73494ebc6954548c4a794ddb7b962336ff2f
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-04-09 00:36:59 -0500

    websocket
    integrate websocket to de server

---
+++ src/main/java/edu/eci/arsw/eci_stream/domain/WSChatMessage.java
@@ -0,0 +1,33 @@
+package edu.eci.arsw.eci_stream.domain;
+
+
+public class WSChatMessage {
+    
+    private String type;
+    private String content;
+    private String sender;
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    public String getSender() {
+        return sender;
+    }
+    
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
--- src/main/resources/static/js/stream.js
+++ src/main/resources/static/js/stream.js
@@ -1,6 +1,7 @@
 var streamModule = (function () {
     
-    var url = location.href;
+    var sala = null;
+    var user = null;
 
     function getUrlVars() {
         var vars = {};
@@ -12,24 +13,71 @@ var streamModule = (function () {
 
     var getRoom = function () {
         APIModule.getRoomById( getUrlVars()['id'], function(data) {
-            console.log(data);            
             $("#data-teacher").append(data.teacher.name);
             $(".title-room").html(data.information.title);
             getUsers(data.users);
         });
         //setTimeout(getRoom, 1000);
-    }
+    };
 
     var getUsers = function (data) {
-        console.log(data);
-        data.forEach( value => {                
+        data.forEach( value => {
             $("#data-users").append("<tr><td>" + value.name + "</td></tr>");
         });
+    };
+    
+    var wsConnect = function () {
+        APIModule.getCurrentUserId(function (data) {
+            user = data;
+        });
+        sala = "/topic/chat." + getUrlVars()['id'];
+        connectAndSubscribe();
+    };
+    
+    var connectAndSubscribe = function () {
+        console.info('Connecting to WS...');
+        var socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+        
+        stompClient.connect({}, connectionSuccess);
+    };
+    
+    function connectionSuccess(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe(sala, onMessage);
+        var message = {
+            author: user.name,
+            type: 'Join'
+        };
+        stompClient.send(sala, {}, JSON.stringify(message));
+    }
+    
+    function sendMessage() {
+        var message = {
+            author: user.name,
+            content: document.querySelector('#chatMessage').value,
+            type: 'Message'
+        };
+        stompClient.send(sala, {}, JSON.stringify(message));
+    }
+    
+    function onMessage(event){
+        var message = JSON.parse(event.body);
+        if (message.type === 'Join')
+            getRoom();        
+        Console.log(message);
     }
 
     return {
         infoRoom: function () {
             getRoom();
+        },
+        init: function () {
+            wsConnect();
+            getRoom();
+        },
+        send: function () {
+            sendMessage();
         }
-    }
+    };
 })();
