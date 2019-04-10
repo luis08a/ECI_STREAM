efd25a94a0038f995b6856e0c4389728f6b3f635
luis <luis.moreno-a@mail.escuelaing.edu.co>
2019-04-09 19:49:20 -0500

    Merge frond
    Co-authored-by: NigDra <andresvilla990223@gmail.com>
    Co-authored-by: Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>

--- src/main/java/edu/eci/arsw/eci_stream/security/AuthProvider.java
+++ src/main/java/edu/eci/arsw/eci_stream/security/AuthProvider.java
@@ -1,6 +1,5 @@
 package edu.eci.arsw.eci_stream.security;
 
-import javax.validation.constraints.Email;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -11,9 +10,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 import edu.eci.arsw.eci_stream.persistence.UserPersistence;
-import edu.eci.arsw.eci_stream.persistence.persistenceImpl.MockPersistence;
-import edu.eci.arsw.eci_stream.persistence.persistenceImpl.dataBaseException;
-import edu.eci.arsw.eci_stream.persistence.persistenceImpl.dataBasePersistance;
 
 @Service
 public class AuthProvider implements AuthenticationProvider {
