8027bdf92bbf8c4335aac039c10d2cd8471e6bde
luis <luis.moreno-a@mail.escuelaing.edu.co>
2019-04-25 18:34:12 -0500

    Funciona stream video
    Co-authored-by: NigDra <andresvilla990223@gmail.com>
    Co-authored-by: NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>

--- src/main/resources/static/js/streamBase.js
+++ src/main/resources/static/js/streamBase.js
@@ -9,7 +9,7 @@ const config = { // eslint-disable-line no-unused-vars
 
 var connectAndSubscribe = function () {
 	console.info('Connecting to WS...');
-	var socket = new SockJS('/stompendpoint');
+	var socket = new SockJS('/ws');
 	stompClient = Stomp.over(socket);
 	
 	stompClient.connect({}, function (frame) {
@@ -29,8 +29,12 @@ var connectAndSubscribe = function () {
     };
     */
 		stompClient.subscribe("/topic/newVideo", function (data) {
-      var i = $("img")
-      i.src = data.image;
+      console.log("success")
+      const i = document.querySelector('img');
+      console.log(i)
+      console.log("----------------------")
+      console.log(data)
+      i.src = data.body;
 		});
 	});
 
@@ -40,22 +44,24 @@ function broadcast() {  // eslint-disable-line no-unused-vars
 	const video = document.querySelector('video');
 	const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
-  navigator.getUserMedia = navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         null;
-	navigator.getUserMedia({ video : true },
+  /*navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia ||
+                         navigator.mediaDevices.webkitGetUserMedia ||
+                         navigator.mediaDevices.mozGetUserMedia ||
+                        null;*/
+  navigator.mediaDevices.getDisplayMedia({ video: true})
+  .then(
 	function(stream) {
 		video.srcObject = stream;
 		setInterval(function() {
 			canvas.width = video.videoWidth;
 			canvas.height = video.videoHeight;
 			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-			const imageData = canvas.toDataURL("image/jpeg", 0.4);
-			stompClient.send("topic/newVideo",{},imageData);
-		}, 100);
-	},function(err) {
-    console.log(err.name + ": " + err.message);
+      const imageData = canvas.toDataURL("image/jpeg", 0.3);
+      //console.log(imageData);
+			stompClient.send("/topic/newVideo",{},imageData);
+    }, 10);
+	}).catch(function(err) {
+    console.log(err.message);
   });
 }
 
